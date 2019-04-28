1ccceff54940a785e925213ca05d011946a811ef
cflamant <cflamant@gmail.com>
2019-04-27 21:48:10 -0400

    Extend chunk_data to timeseries of feature vectors
    -chunk_data() accepts both timeseries of scalars and of feature vectors
    -fix bug in computing num_chunks in chunk_data()
    -add tests of chunk_data()

--- parallel_esn/tests/test_utils.py
+++ parallel_esn/tests/test_utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pytest
-from ..utils import compute_spectral_radius, create_rng
+from ..utils import compute_spectral_radius, create_rng, chunk_data
 
 
 def test_compute_spectral_radius():
@@ -40,3 +40,48 @@ def test_create_rng():
     rng1 = create_rng(np.random.RandomState(17))
     bytes1 = rng1.bytes(1)
     assert bytes0 == bytes1
+
+
+def test_chunk_data_1d():
+    # Test chunking 1d data
+    timeseries = np.array([0, 1, 2, 3, 4, 5, 6, 7])
+
+    # Test when data chunks evenly
+    chunkU1, chunkY1 = chunk_data(timeseries, 2, 4)
+    ansU1 = np.array([[[0, 1]], [[4, 5]]])
+    ansY1 = np.array([[[2, 3]], [[6, 7]]])
+    np.testing.assert_array_equal(chunkU1, ansU1)
+    np.testing.assert_array_equal(chunkY1, ansY1)
+
+    # Test when data does not chunk evenly
+    chunkU2, chunkY2 = chunk_data(timeseries, 2, 3)
+    ansU2 = np.array([[[0, 1]], [[3, 4]]])
+    ansY2 = np.array([[[2, 3]], [[5, 6]]])
+    np.testing.assert_array_equal(chunkU2, ansU2)
+    np.testing.assert_array_equal(chunkY2, ansY2)
+
+
+def test_chunk_data_2d():
+    # Test chunking 2d data
+    timeseries = np.array([[0, 0],
+                           [1, -1],
+                           [2, -2],
+                           [3, -3],
+                           [4, -4],
+                           [5, -5],
+                           [6, -6],
+                           [7, -7]])
+
+    # Test when data chunks evenly
+    chunkU1, chunkY1 = chunk_data(timeseries, 2, 4)
+    ansU1 = np.array([[[0, 1], [0, -1]], [[4, 5], [-4, -5]]])
+    ansY1 = np.array([[[2, 3], [-2, -3]], [[6, 7], [-6, -7]]])
+    np.testing.assert_array_equal(chunkU1, ansU1)
+    np.testing.assert_array_equal(chunkY1, ansY1)
+
+    # Test when data does not chunk evenly
+    chunkU2, chunkY2 = chunk_data(timeseries, 2, 3)
+    ansU2 = np.array([[[0, 1], [0, -1]], [[3, 4], [-3, -4]]])
+    ansY2 = np.array([[[2, 3], [-2, -3]], [[5, 6], [-5, -6]]])
+    np.testing.assert_array_equal(chunkU2, ansU2)
+    np.testing.assert_array_equal(chunkY2, ansY2)
--- parallel_esn/utils.py
+++ parallel_esn/utils.py
@@ -56,30 +56,49 @@ def chunk_data(timeseries, windowsize, stride):
     Parameters
     ----------
     timeseries : np.ndarray
-        Time series data
+        Time series data, which can be 1D or 2D. If 2D, first index
+        corresponds to the time, and the second index corresponds to
+        a feature in the feature vector. I.e. feature vectors are
+        rows stacked vertically in time.
     windowsize : int
-        The number of data points to be used as input (equal to expected number
-        of outputs)
+        The number of data points in time to be used as input (equal to
+        expected number of outputs)
     stride : int
         Number of points to shift in time to generate a subsequent input
         window/output window of data.
     Returns
     -------
     batchU : np.ndarray
-        Partitioned samples
+        Partitioned samples.
+        Dimensions are (num_samples, num_features, windowsize)
     batchY : np.ndarray
-        Partitioned targets
+        Partitioned targets.
+        Dimensions are (num_samples, num_features, windowsize)
 
     """
+    ts_dim = len(timeseries.shape)
+    if ts_dim > 2 or ts_dim < 1:
+        raise ValueError(("Improper shape passed to chunk_data, "
+                          "timeseries must be one or two dimensional. (Received "
+                          "timeseries with dimension = {}".format(ts_dim)))
+    elif ts_dim == 2:  # feature vector
+        feature_len = timeseries.shape[1]
+    else:  # single feature
+        feature_len = 1
+        # promote to 2D array
+        timeseries = timeseries.reshape(-1, 1)
+
     length = timeseries.shape[0]
-    num_chunks = (length-(2*windowsize - 1))//stride
-    batchU = np.zeros((num_chunks, 1, windowsize))
-    batchY = np.zeros((num_chunks, 1, windowsize))
+    # Get transposed view for later array assignment convenience
+    timeseriesT = timeseries.T
+    num_chunks = (length - 2*windowsize)//stride + 1
+    batchU = np.zeros((num_chunks, feature_len, windowsize))
+    batchY = np.zeros((num_chunks, feature_len, windowsize))
     for i in range(num_chunks):
         start = stride*i
         end = start + windowsize
-        batchU[i, 0, :] = timeseries[start:end]
+        batchU[i, :, :] = timeseriesT[:, start:end]
         start = stride*i + windowsize
         end = start + windowsize
-        batchY[i, 0, :] = timeseries[start:end]
+        batchY[i, :, :] = timeseriesT[:, start:end]
     return batchU, batchY
