4f702ac788387d0d6550dc74f3ca74d281f3d154
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-05-04 23:36:36 +0000

    Update docs after building Travis build 25 of zblanks/parallel_esn
    The docs were built from the branch 'master' against the commit
    323fcc6dcba8d3ac91a59c106aea5eb377c72b64.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/zblanks/parallel_esn/jobs/528298660.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post41+g3cff114',
+    VERSION: '0.post42+g323fcc6',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
